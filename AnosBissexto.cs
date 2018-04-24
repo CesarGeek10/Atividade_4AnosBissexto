2dc22dfb0808bc9207139d95403666a4026e097f
CesarGeek <cesar.work10@gmail.com>
2018-04-23 23:01:08 -0300

    Atividade sobre os Anos Bissextos

---
+++ AnosBissexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Olá, Esse é o Software Que Indicará os Anos Bissexto");
+
+            int qtd=0;
+            for(int i=1910; i <=2018; i++){
+                if(i%4==0){
+                    Console.WriteLine(i + " - Este Ano é Bissexto");
+                    qtd++;
+
+                }
+            }
+
+            Console.WriteLine(qtd + " - Anos Bissexto");
+        }
+    
+    }
+}
